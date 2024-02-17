eb9f19980ad0e8f7e23b489589cae73a978b580f
Anton Krutoy <2191@mail.ru>
2024-02-17 14:26:03 +0500

    remove code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ fun main() {
     val likes = 49
     val result = when (likes % 10) {
         1 -> "Понравилось $likes человеку"
-        in 2..4 -> "Понравилось $likes людям"
         else -> "Понравилось $likes людям"
     }
     println(result)
