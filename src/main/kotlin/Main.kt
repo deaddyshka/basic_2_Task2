44c0d8453394f855dcc0f621240c877074a51dc3
Anton Krutoy <2191@mail.ru>
2024-02-19 18:18:19 +0500

    write when

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 fun main() {
-    val likes = 49
+    val likes = 11
     val result = when (likes % 10) {
         1 -> "Понравилось $likes человеку"
+        11 -> "Понравилось $likes людям"
         else -> "Понравилось $likes людям"
     }
     println(result)
