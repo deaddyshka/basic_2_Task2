cc145f2bdd0b1331cc27bdc85323a93ea21c8499
Anton Krutoy <2191@mail.ru>
2024-02-19 19:46:18 +0500

    211 311 1011 правки

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 fun main() {
-    val likes = 11
-    val result = when (likes % 10) {
-        1 -> "Понравилось $likes человеку"
-        11 -> "Понравилось $likes людям"
+    val likes = 1111
+    val result = when {
+        likes % 100 == 11 -> "Понравилось $likes людям"
+        likes % 10 == 1 -> "Понравилось $likes человеку"
         else -> "Понравилось $likes людям"
     }
     println(result)
